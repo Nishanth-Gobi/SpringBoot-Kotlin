32b9864ee39b3bb24b7d7b587c12db25500653d0
Nish <nish.professional@gmail.com>
2024-04-20 22:15:42 +0530

    controller without responseBody

--- src/main/kotlin/starter/main/content/StarterController.kt
+++ src/main/kotlin/starter/main/content/StarterController.kt
@@ -2,14 +2,12 @@ package starter.main.content
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class StarterController {
 
     @GetMapping("/")
-    @ResponseBody
     fun sayHello(): String {
-        return "Hello!"
+        return "hello.html"
     }
 }
