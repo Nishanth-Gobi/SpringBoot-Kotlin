10075280f2670ca73d7c5d773b18745d73904f9b
Nish <nish.professional@gmail.com>
2024-04-21 11:55:38 +0530

    restController - SpringBootTest & mockMvc - with Service - integration test

--- src/test/kotlin/starter/main/content/StarterControllerTest.kt
+++ src/test/kotlin/starter/main/content/StarterControllerTest.kt
@@ -1,11 +1,9 @@
 package starter.main.content
 
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
@@ -15,14 +13,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @SpringBootTest
 @AutoConfigureMockMvc
 class StarterControllerTest(@Autowired val mockMvc: MockMvc) {
-//    Integration test
-    @MockBean
-    private lateinit var starterService: StarterService
-
+//  Integration test
     @Test
     fun shouldReturnHello() {
-
-        `when`(starterService.getMessage()).thenReturn("hello!")
 
         this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
             .andDo(print())
