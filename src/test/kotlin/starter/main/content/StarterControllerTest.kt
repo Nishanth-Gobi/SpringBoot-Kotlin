e37554c260ca377606e273678e95146145cd630e
Nish <nish.professional@gmail.com>
2024-04-21 12:36:53 +0530

    restController - WebMvcTest & mockMvc - with Service - integration test

--- src/test/kotlin/starter/main/content/StarterControllerTest.kt
+++ src/test/kotlin/starter/main/content/StarterControllerTest.kt
@@ -1,27 +1,20 @@
 package starter.main.content
 
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@WebMvcTest
+@WebMvcTest(StarterController::class, StarterService::class)
 class StarterControllerTest(@Autowired val mockMvc: MockMvc) {
-//  Unit test
-
-    @MockBean
-    private lateinit var starterService: StarterService
+//  Integration test
 
     @Test
     fun shouldReturnHello() {
-        `when`(starterService.getMessage()).thenReturn("hello!")
-
         this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
             .andDo(print())
             .andExpect(status().isOk)
